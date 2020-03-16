6042a4ff30220be3548369a9b24e74abb094d336
jborza <jborza@gmail.com>
2020-03-16 21:15:26 +0100

    characters model in model fix

--- model.js
+++ model.js
@@ -2,7 +2,7 @@ fonts = ["Arial", "Times New Roman", "Courier New", "Comic Sans MS"];
 
 function create_characters(max_characters) {
     const FIRST_CHARCODE = 32;
-    return [...Array(max_characters - FIRST_CHARCODE)].map(i => String.fromCharCode(i + FIRST_CHARCODE));
+    return [...Array(max_characters - FIRST_CHARCODE).keys()].map(i => String.fromCharCode(i + FIRST_CHARCODE));
 }
 
 function create_charmap_model() {
