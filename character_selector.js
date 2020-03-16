fea0e57bbab83fe04d1c6c31859a5bd3f5bc9cc3
jborza <jborza@gmail.com>
2020-03-16 21:59:33 +0100

    displaying clicked character

--- character_selector.js
+++ character_selector.js
@@ -2,8 +2,16 @@
 
 function create_character_selector_viewstate() {
     return {
-        selected_character: undefined
+        selected_character: undefined,
+        code_input: undefined,
+        glyph_input: undefined
     }
+}
+
+function on_character_clicked(vs, parameters) {
+    console.log("on_character_clicked", parameters);
+    vs.selected_character = parameters;
+    character_selector_render(vs);
 }
 
 function character_selector_layout(vs, root_container) {
@@ -14,22 +22,31 @@ function character_selector_layout(vs, root_container) {
     let value_container = create_container();
     let value_label = create_label_with_text("Glyph:");
     add_child(value_container, value_label);
-    let selected_input = create_input();
-    add_child(value_container, selected_input);
+    vs.glyph_input = create_input();
+    add_child(value_container, vs.glyph_input);
     add_child(container, value_container);
 
     let code_container = create_container();
     let code_label = create_label_with_text("Code:");
     add_child(code_container, code_label);
-    let code_input = create_input();
-    add_child(code_container, code_input);
+    vs.code_input = create_input();
+    add_child(code_container, vs.code_input);
     add_child(container, code_container);
 
     //TODO add description
 
     add_child(root_container, container);
+
+    connect_signal(SIGNAL_CHARACTER_CLICKED, on_character_clicked, vs);
+}
+
+function hexcode(str) {
+    return str.charCodeAt(0).toString(16).toUpperCase().padStart(4, '0');
 }
 
 function character_selector_render(vs) {
     //U+0123 + description
+    set_value(vs.glyph_input, vs.selected_character);
+    let code = "U+" + hexcode(vs.selected_character)
+    set_value(vs.code_input, code);
 }
--- charmap_grid.js
+++ charmap_grid.js
@@ -1,7 +1,15 @@
+const SIGNAL_CHARACTER_CLICKED = "SIGNAL_CHARACTER_CLICKED";
+
 function create_charmap_grid_viewstate(characters) {
     return {
         characters: characters
     }
+}
+
+function character_clicked_handler(vs, target) {
+    let el = target.target;
+    let value = el.value;
+    raise_signal(SIGNAL_CHARACTER_CLICKED, value);
 }
 
 function charmap_grid_layout(vs, root_container) {
@@ -12,6 +20,7 @@ function charmap_grid_layout(vs, root_container) {
     for (const character of vs.characters) {
         let button = create_element("input");
         set_attribute(button, "type", "button");
+        add_click_handler(button, character_clicked_handler, vs);
         set_value(button, character);
         add_child(container, button);
     }
