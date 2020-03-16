d2cc97a8b63980dd48031bfbd283971c3b65661b
jborza <jborza@gmail.com>
2020-03-16 21:15:05 +0100

    JS component template

---
+++ _template/_component.js
@@ -0,0 +1,9 @@
+//replace _COMPONENT_ with component name
+
+function create__COMPONENT__viewstate() {
+    return {}
+}
+
+function _COMPONENT__layout(vs, root_container) {}
+
+function _COMPONENT__render(vs) {}
